27ab4f4636a128cc8595e2311e16f6ab891af8e1
dener8 <8dener@gmail.com>
2022-10-25 23:35:13 +0900

    221025

---
+++ boj25304.py
@@ -0,0 +1,11 @@
+# 25304
+price = int(input())
+n = int(input())
+sum = 0
+for i in range(n):
+    a, b = map(int,input().split())
+    sum += a*b
+if price == sum:
+    print('Yes')
+else:
+    print('No')
